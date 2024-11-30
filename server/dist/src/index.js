d1b0df1bbf1538fa8d12cafa9a0b2a16669f9967
pervej0 <mdparvez222khan@gmail.com>
2024-11-30 16:09:14 +0600

    starting server

---
+++ server/dist/src/index.js
@@ -0,0 +1,26 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { db } from "../db";
+const typeDefs = `#graphql
+    type Book {
+        title: String
+        author: String
+    }
+
+   type Query {
+    books: [Book]
+  }
+`;
+const resolvers = {
+    Query: {
+        books: () => db.books,
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
